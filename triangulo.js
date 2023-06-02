c238bf4824ce7470dbe0418b1ec3174ad4ddd4f0
Tomas <112868952+TomyReiv@users.noreply.github.com>
2023-06-02 11:16:23 -0300

    commit ayer tarde

---
+++ triangulo.js
@@ -0,0 +1,11 @@
+/* a + b > c, a + c > b y b + c > a */
+
+function isTriangle(a,b,c){
+    
+    if(a + b > c && a + c > b && b + c > a){
+        return true;
+    } else {
+        return false;
+    }
+   
+}
